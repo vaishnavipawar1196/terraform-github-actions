de55380f6f2b94342658e372a227d13955c32caf
Vaishnavi Vijay Pawar <59501315+vaishnavipawar1196@users.noreply.github.com>
2026-07-21 16:13:38 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   required_version = ">=1.5"
 
@@ -8,15 +7,4 @@ terraform {
       version = "~>6.0"
     }
   }
-=======
-terraform {
-  required_version = ">=1.5"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>6.0"
-    }
-  }
->>>>>>> 3677fb0a947986853df2041708057f7db7f17b65
 }
