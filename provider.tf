c6e6adbca7bedfe235eeb4d855a1bb8d051f531a
Vaishnavi Vijay Pawar <59501315+vaishnavipawar1196@users.noreply.github.com>
2026-07-21 16:19:16 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 provider "aws" {
   region = var.region
-=======
-provider "aws" {
-  region = var.region
->>>>>>> 3677fb0a947986853df2041708057f7db7f17b65
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 variable "region" {
   default = "ap-south-1"
 }
 
 variable "instance_type" {
   default = "t3.micro"
-=======
-variable "region" {
-  default = "ap-south-1"
-}
-
-variable "instance_type" {
-  default = "t3.micro"
->>>>>>> 3677fb0a947986853df2041708057f7db7f17b65
 }
